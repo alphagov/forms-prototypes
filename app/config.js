b95fe2fe640d90eea71089b2c8d518712f86747a
Tim Paul <contact@timpaul.co.uk>
2021-09-29 10:18:17 +0100

    Add and style index pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Form examples',
+  serviceName: 'CIFU Prototypes',
 
   // Default port that prototype runs on
   port: '3000',
