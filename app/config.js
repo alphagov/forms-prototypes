2b1f26a55bebb37d337c8959e05dc3720b260ba7
James Sheppard <57200592+james-sheppard-gov-uk@users.noreply.github.com>
2022-02-03 11:08:31 +0000

    Updated reference to CIFU (#20)

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'CIFU Prototypes',
+  serviceName: 'GOV.UK Forms Prototypes',
 
   // Default port that prototype runs on
   port: '3000',
