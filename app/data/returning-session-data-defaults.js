7c00d08fe36006e7dc10a12a77789a39fb1d8cd6
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-24 12:16:03 +0100

    Fix issue with highestPageId

--- app/data/returning-session-data-defaults.js
+++ app/data/returning-session-data-defaults.js
@@ -18,59 +18,61 @@ Example usage:
 */
 
 module.exports = {
-  "highestPageId": 5,
-  "action": "update",
-  "publish": "GOV.UK",
-  "authentication": "email",
-  "payments": "no",
-  "pages": [
+  highestPageId: 6,
+  action: 'update',
+  publish: 'GOV.UK',
+  authentication: 'email',
+  payments: 'no',
+  pages: [
     {
-      "intro": "This is the intro",
-      "long-title": "What is you name?",
-      "short-title": "Name",
-      "hint-text": "Enter your fuul name",
-      "type": "text",
-      "pageIndex": "0"
+      intro: 'This is the intro',
+      'long-title': 'What is you name?',
+      'short-title': 'Name',
+      'hint-text': 'Enter your fuul name',
+      type: 'text',
+      pageIndex: '0'
     },
     {
-      "long-title": "What is your claim reference number?",
-      "short-title": "Claim reference number",
-      "hint-text": "Begings with LN",
-      "type": "text",
-      "pageIndex": "1"
+      'long-title': 'What is your claim reference number?',
+      'short-title': 'Claim reference number',
+      'hint-text': 'Begings with LN',
+      type: 'text',
+      pageIndex: '1'
     },
     {
-      "long-title": "What is your national insurance number?",
-      "short-title": "National Insurance number",
-      "hint-text": "For example QQ12345C",
-      "type": "text",
-      "pageIndex": "2"
+      'long-title': 'What is your national insurance number?',
+      'short-title': 'National Insurance number',
+      'hint-text': 'For example QQ12345C',
+      type: 'text',
+      pageIndex: '2'
     },
     {
-      "long-title": "What is your date of bitrh?",
-      "short-title": "Date of Bitrh",
-      "hint-text": "For example 27 3 2007",
-      "type": "address",
-      "pageIndex": "3"
+      'long-title': 'What is your date of bitrh?',
+      'short-title': 'Date of Bitrh',
+      'hint-text': 'For example 27 3 2007',
+      type: 'address',
+      pageIndex: '3'
     },
     {
-      "long-title": "When did you submit your redundancy claim?",
-      "short-title": "Claim submitted",
-      "hint-text": "For example 27 3 2007",
-      "type": "date",
-      "pageIndex": "4"
+      'long-title': 'When did you submit your redundancy claim?',
+      'short-title': 'Claim submitted',
+      'hint-text': 'For example 27 3 2007',
+      type: 'date',
+      pageIndex: '4'
     },
     {
-      "long-title": "What is your adress?",
-      "short-title": "Address",
-      "type": "date",
-      "pageIndex": "5"
+      'long-title': 'What is your adress?',
+      'short-title': 'Address',
+      type: 'date',
+      pageIndex: '5'
     }
   ],
-  "status": "Draft",
-  "confirmationTitle": "Form submitted",
-  "confirmationNext": "We've sent you an email to confirm we have received your form.",
-  "checkAnswersTitle": "Check your answers",
-  "checkAnswersDeclaration": "By submitting this form you are confirming that, to the best of your knowledge, the answers you are providing are correct.",
-  "formTitle": "Redundancy payments form: amend my personal details"
+  status: 'Draft',
+  confirmationTitle: 'Form submitted',
+  confirmationNext:
+    "We've sent you an email to confirm we have received your form.",
+  checkAnswersTitle: 'Check your answers',
+  checkAnswersDeclaration:
+    'By submitting this form you are confirming that, to the best of your knowledge, the answers you are providing are correct.',
+  formTitle: 'Redundancy payments form: amend my personal details'
 }
