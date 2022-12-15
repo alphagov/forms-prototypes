b79e74738b83725f9d6790d443b3f0918455ea1b
Chris Cameron <chris.cameron@digital.cabinet-office.gov.uk>
2022-12-15 16:11:36 +0000

    Added notification banners and messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,14 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// remove notification banner after X seconds
+function removeSuccessNotification() {
+  // get the banner by class (also covers if we accidentally have more than 1 appear)
+  const elems = document.querySelectorAll('.govuk-notification-banner--success');
+  setTimeout(function() {
+    for (const e of elems) {
+      e.remove();
+    }
+  }, 10000); // 10 seconds
+}
