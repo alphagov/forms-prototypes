57ef1334aea0f1f338e74003890a3b8b04b69eb5
Christopher Cameron <35372982+christophercameron-ixd@users.noreply.github.com>
2024-05-28 14:45:40 +0100

    Add comma format for long numbers

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,33 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+addFilter('commaFormat', function (inputString) {
+    inputString = inputString.toString();
+    var decimalPart = "";
+    if (inputString.indexOf('.') != -1) {
+        //alert("decimal number");
+        inputString = inputString.split(".");
+        decimalPart = "." + inputString[1];
+        inputString = inputString[0];
+        //alert(inputString);
+        //alert(decimalPart);
+
+    }
+    var outputString = "";
+    var count = 0;
+    for (var i = inputString.length - 1; i >= 0 && inputString.charAt(i) != '-'; i--) {
+        //alert("inside for" + inputString.charAt(i) + "and count=" + count + " and outputString=" + outputString);
+        if (count == 3) {
+            outputString += ",";
+            count = 0;
+        }
+        outputString += inputString.charAt(i);
+        count++;
+    }
+    if (inputString.charAt(0) == '-') {
+        outputString += "-";
+    }
+    //alert(outputString);
+    //alert(outputString.split("").reverse().join(""));
+    return outputString.split("").reverse().join("") + decimalPart;
+})
